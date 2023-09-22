4f83f651aaefbd4d74894f6bc42fcdf40f8dfe13
Reda Grain <97684199+grain03@users.noreply.github.com>
2023-09-22 15:10:16 +0100

    Update PHP-POO.php

--- PHP-POO.php
+++ PHP-POO.php
@@ -1,21 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
 class Car {
   public $brand;
   public $model;
+  public $price;
 
-  function set_model($model){
+  function __construct($model, $price){
     $this->model = $model;
+    $this->price = $price;
   }
 
   function get_model(){
     return $this->model;
+  }  
+  
+  function get_price(){
+    return $this->price;
   }
 }
 
 
-$Volkswagen  = new Car();
-$Volkswagen->set_model("Passat");
+$Volkswagen  = new Car("Passat", "7399$");
+$Peugeot  = new Car("5008", "5500$");
 
 
-echo $Volkswagen->get_model();
+echo $Volkswagen->get_model() . " Costs " . $Volkswagen->get_price() . " And " .  $Peugeot->get_model() . " Costs " . $Peugeot->get_price();
 ?>
+</body>
+</html>
